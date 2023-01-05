fe114922add7abfeb95d1e03ab467cadd5b25cf9
amassock <ajmassock@gmail.com>
2023-01-04 20:12:08 -0600

    complete prompt

--- JavaScript/January/01_04_Min_Number/script.js
+++ JavaScript/January/01_04_Min_Number/script.js
@@ -3,7 +3,22 @@
  * @return {number}
  */
 var findMinArrowShots = function(points) {
-    
+    points.sort((a, b) => a[0] - b[0]);
+
+    let darts = 1;
+    let rightOverlap = points[0][1];
+
+    for(let i = 1; i < points.length; i++) {
+        if(points[i][0] > rightOverlap) {
+            darts++
+            rightOverlap = points[i][1]
+            continue
+        } else if(points[i][1] <= rightOverlap) {
+            rightOverlap = Math.min(rightOverlap, points[i][1])
+        }
+    }
+    console.log(darts);
+    return darts
 };
 
 
