203644a7551ee74590dfd15cdd4132fa079f29b6
amassock <ajmassock@gmail.com>
2023-01-04 20:01:03 -0600

    skeleton for problem

---
+++ JavaScript/January/01_04_Min_Number/script.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    
+};
+
+
+
+// Tests
+// points = [[10,16],[2,8],[1,6],[7,12]];
+// points = [[1,2],[3,4],[5,6],[7,8]];
+// points = [[1,2],[2,3],[3,4],[4,5]];
+
+// findMinArrowShots(points);
