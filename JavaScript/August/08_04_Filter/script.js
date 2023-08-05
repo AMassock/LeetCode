65108400983dde2c4f72563c8fa80f25e257789e
amassock <ajmassock@gmail.com>
2023-08-04 23:05:23 -0500

    Complete August 4th's challenge

--- JavaScript/August/08_04_Filter/script.js
+++ JavaScript/August/08_04_Filter/script.js
@@ -3,4 +3,14 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var filter = function (arr, fn) {};
+var filter = function (arr, fn) {
+  let filteredArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      filteredArr.push(arr[i]);
+    }
+  }
+
+  return filteredArr;
+};
