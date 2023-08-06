05c10a0c16fd3d3147fa40c31d799e57fa8b3271
amassock <ajmassock@gmail.com>
2023-08-05 22:33:14 -0500

    Complete August 5th's challenge

--- JavaScript/August/08_05_Array_Reduce/script.js
+++ JavaScript/August/08_05_Array_Reduce/script.js
@@ -4,4 +4,9 @@
  * @param {number} init
  * @return {number}
  */
-var reduce = function (nums, fn, init) {};
+var reduce = function (nums, fn, init) {
+  for (let i = 0; i < nums.length; i++) {
+    init = fn(init, nums[i]);
+  }
+  return init;
+};
